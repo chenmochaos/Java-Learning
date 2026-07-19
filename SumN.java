283e8322080b13736bce8133922be797c6056fe5
Minus <deeegolden184@gmail.com>
2026-07-19 11:58:23 +0800

    Finish HelloWorld & SumN

---
+++ SumN.java
@@ -0,0 +1,11 @@
+public class SumN {
+    // 计算前N个自然数的和
+    public static void main(String[] args) {
+        int n = 100;
+        // TODO: sum = 1 + 2 + ... + n
+        int sum = n*(n+1)/2;
+        System.out.println(sum);
+        System.out.println(sum == 5050 ? "测试通过" : "测试失败");
+    }
+
+}
