5c8861fc2f68cf57892982220c1884b1862fd1c6
Minus <deeegolden184@gmail.com>
2026-07-19 13:13:57 +0800

    IsPrimaryStu&UnicodeStr

---
+++ IsPrimaryStu.java
@@ -0,0 +1,9 @@
+public class IsPrimaryStu {
+    public static void main(String[] args) {
+        int age = 7;
+        // primary student的定义: 6~12岁
+        boolean isPrimaryStudent = age <= 12 && age >= 6 ? true: false;
+        System.out.println(isPrimaryStudent ? "Yes" : "No");
+    }
+}
+    
---
+++ UnicodeStr.java
@@ -0,0 +1,11 @@
+public class UnicodeStr {
+    public static void main(String[] args) {
+        // 请将下面一组int值视为字符的Unicode码，把它们拼成一个字符串：
+        int a = 72;
+        int b = 105;
+        int c = 65281;
+        // 把 int 当作 Unicode 码点，转换成字符后拼接成字符串
+        String s = "" + (char) a + (char) b + (char) c;
+        System.out.println(s);
+    }
+}
