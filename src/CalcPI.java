abccca8e151546df7973494054b4381d214864b3
Minus <deeegolden184@gmail.com>
2026-07-19 17:41:36 +0800

    CalcPI&BubbleSort

---
+++ src/CalcPI.java
@@ -0,0 +1,11 @@
+public class CalcPI {
+    public static void main(String[] args) {
+        double pi = 0;
+        int flag = -1;
+        for (int i = 1; i < 1_000_000; i += 2) {
+            flag *= -1;
+            pi += (1.0 / i) * flag;
+        }
+        System.out.println(4 * pi);
+    }
+}
